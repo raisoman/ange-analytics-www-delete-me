bceb286496e18d110da9f22f7cd6ef4f9eb89f29
Nicolas Guilbert <nicolas@ange.dk>
2016-02-10 10:52:54 +0100

    Stub descriptions + new front page

--- js/addheader.js
+++ js/addheader.js
@@ -2,24 +2,22 @@ var addHeader = function() {
     document.getElementsByClassName('mdl-layout')[0].innerHTML +=
     '<header class="mdl-layout__header">\
         <div class="mdl-layout__header-row">\
-            <span class="mdl-layout-title"><a href="index.html"><img src="images/linergame-logo.png" alt="LinerGame"></a></span>\
+            <span class="mdl-layout-title"><a href="index.html"><img src="images/ange-logo.png" alt="Ange"></a></span>\
         </div>\
     </header>\
     <div class="mdl-layout__drawer">\
-        <span class="mdl-layout-title">LinerGame</span>\
+        <span class="mdl-layout-title">Ange</span>\
         <nav class="mdl-navigation">\
-            <a class="mdl-navigation__link" href="index.html">Home (The Game)</a>\
-            <a class="mdl-navigation__link" href="about.html">About</a>\
-            <a class="mdl-navigation__link" href="references.html">References</a>\
-            <a class="mdl-navigation__link" href="contact.html">Contact</a>\
+            <a class="mdl-navigation__link" href="index.html">Home</a>\
+            <a class="mdl-navigation__link" href="about.html">About us</a>\
             <div class="drawer-separator"></div>\
-            <span class="mdl-navigation__link mdl-color-text--cyan-400" href="">Game types</span>\
-            <a class="mdl-navigation__link" href="standard-game.html">Standard</a>\
-            <a class="mdl-navigation__link" href="executive-game.html">Executive</a>\
-            <a class="mdl-navigation__link" href="support-functions-game.html">Support functions</a>\
-            <a class="mdl-navigation__link" href="teambuilding-game.html">Teambuilding</a>\
-            <a class="mdl-navigation__link" href="ports-game.html">For ports</a>\
-            <a class="mdl-navigation__link" href="advanced-game.html">Advanced</a>\
+            <span class="mdl-navigation__link mdl-color-text--cyan-400" href="">Stories</span>\
+            <a class="mdl-navigation__link" href="business-intelligence.html">Business Intelligence</a>\
+            <a class="mdl-navigation__link" href="analytics.html">Analytics</a>\
+            <a class="mdl-navigation__link" href="software-development.html">Software Development</a>\
+            <a class="mdl-navigation__link" href="process-improvement.html">Process Improvement</a>\
+            <a class="mdl-navigation__link" href="linova.html">Network Optimization</a>\
+            <a class="mdl-navigation__link" href="stowage.html">Stowage</a>\
         </nav>\
     </div>';
 }
--- js/addproductcards.js
+++ js/addproductcards.js
@@ -44,13 +44,13 @@ var addProductCards = function() {
             </div>\
             <div class="demo-card-wide mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-phone is-relative">\
                 <div class="mdl-card__title linercard4">\
-                    <h2 class="mdl-card__title-text">Training</h2>\
+                    <h2 class="mdl-card__title-text">Process Improvement</h2>\
                 </div>\
                 <div class="mdl-card__supporting-text">\
-                    In collaboration with Maersk Line and Aalborg University, we developed the Liner Shipping Game<sup>TM</sub>...\
+                    In collaboration with Maersk Line and Aalborg University, we developed the Liner Shipping Game<sup>TM</sup>...\
                 </div>\
                 <div class="mdl-card__actions in-bottom">\
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="training.html">\
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="process-improvement.html">\
                         Read more\
                     </a>\
                 </div>\
