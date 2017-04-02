f5a597924107d7314f9900a40919136667a9c0d5
Sacha Guilbert <sachaguilbert@gmail.com>
2017-04-02 20:19:47 +0200

    Big data for DW page

--- js/addheader.js
+++ js/addheader.js
@@ -14,6 +14,7 @@ var addHeader = function() {
             <span class="mdl-navigation__link mdl-color-text--cyan-400" href="">Stories</span>\
             <a class="mdl-navigation__link" href="azure-migration.html">Azure Migration</a>\
             <a class="mdl-navigation__link" href="visualization-case.html">Data Visualization</a>\
+            <a class="mdl-navigation__link" href="big-data-for-dw.html">Big data for DW</a>\
             <a class="mdl-navigation__link" href="analytics.html">Analytics</a>\
             <a class="mdl-navigation__link" href="business-intelligence.html">Business Intelligence</a>\
             <a class="mdl-navigation__link" href="process-improvement.html">Process Improvement</a>\
